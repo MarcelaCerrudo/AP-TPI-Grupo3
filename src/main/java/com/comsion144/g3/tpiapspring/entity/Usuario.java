de4a948db303eea7980836d17483dbeaf347a725
javpintos <jpintos234@gmail.com>
2023-11-20 14:59:06 -0300

    Usuario added

--- src/main/java/com/comsion144/g3/tpiapspring/entity/Usuario.java
+++ src/main/java/com/comsion144/g3/tpiapspring/entity/Usuario.java
@@ -2,26 +2,20 @@ package com.comsion144.g3.tpiapspring.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "cliente")
+@Table(name = "usuario")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 
-public class Cliente {
+public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    private String razonSocial;
-
-    private String CUIT;
-
-    private String mail;
+    private String username;
+    private String password;
 
 }
